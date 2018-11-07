f269667cf02ce47b7719e690fc344b416f7a69a5
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-07 16:43:31 -0700

    Adds test for page not existing

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+  
+  def test_user_sees_message_if_page_doesnt_exists
+    visit '/blog'
+    
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
 end
